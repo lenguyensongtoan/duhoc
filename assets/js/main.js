c1730885c699c70bfb53cdbf27ae2695255385a9
toanlns <lenguyensongtoan15598@gmail.com>
2022-05-21 19:47:50 +0700

    Add slick slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
-function trick() {
-    alert('a')
-}
+$(document).ready(function () {
+    $('.homepageCarousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
+
+$(document).ready(function () {
+    $('.video_qa').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+    });
+});
+
+$(document).ready(function () {
+    $('.link_sldr').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+});
