6f683c7e0c1947b31544fd5eb7736d9b602e67ee
toanlns <lenguyensongtoan15598@gmail.com>
2022-06-01 20:19:28 +0700

    Update Review section

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,3 +122,31 @@ $(document).ready(function () {
     $("body").css("overflow", "");
   });
 });
+
+$(document).ready(function () {
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+  
+      1200: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      }
+    }
+  });
+});
